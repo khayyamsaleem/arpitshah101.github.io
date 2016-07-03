791f22dab06a5ba1dbd60aaab804f49e09582351
Arpit Shah <arpitshah101@gmail.com>
2016-07-03 18:58:14 -0400

    bare setup for angular 2 version of website

---
+++ app/app.component.ts
@@ -0,0 +1,6 @@
+import { Component } from '@angular/core';
+@Component({
+  selector: 'my-app',
+  template: '<div class="card">My First Angular 2 App</div><div class="card card-1">My First Angular 2 App</div>'
+})
+export class AppComponent { }
